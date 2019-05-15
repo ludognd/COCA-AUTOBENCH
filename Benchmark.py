a5484f35e310e3ca2eb38c25a402f27dcc227847
David Wittwer <wittwer.david1@gmail.com>
2019-05-15 12:12:17 +0200

    Initial code commit with empty objects

---
+++ Benchmark.py
@@ -0,0 +1,13 @@
+from Instance import Instance
+from Provider import Provider
+
+
+class Benchmark:
+    def __init__(self, provider, instance):
+        """
+        Construct benchmark object
+
+        :param Provider provider:
+        :param Instance instance:
+        """
+        pass
---
+++ coca-bench.py
@@ -0,0 +1,11 @@
+from Benchmark import Benchmark
+from Instance import Instance
+from Provider import Provider
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
