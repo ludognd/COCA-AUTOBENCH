fe5fd11b24f8e136731744493138456ac4dadcfc
Xcaliburne <lu.gindre@gmail.com>
2019-05-15 15:23:23 +0200

    Added ssh keypair generation

--- coca-bench.py
+++ coca-bench.py
@@ -1,10 +1,22 @@
 from Benchmark import Benchmark
 from Instance import Instance
 from Provider import Provider
+from Crypto.PublicKey import RSA
+import argparse
 
 
 def run():
-    pass
+    parser = argparse.ArgumentParser(description='Run benchmarks in all instances defined by the config file')
+    parser.add_argument("providers", help='Configuration file describing providers')
+    parser.add_argument("instances", help='Configuration file describing instances to create and benchmark')
+    parser.add_argument("--nThreads", type=int, help='Number of threads to start')
+    parser.add_argument("--keyLength", type=int, default=2048, help='Length of the generated SSH key')
+    args = parser.parse_args()
+
+    print args
+    print args.keyLength
+    key = RSA.generate(2048)
+    keypair = (key.exportKey('PEM'), key.publickey().exportKey('OpenSSH'))
 
 
 if __name__ == '__main__':
